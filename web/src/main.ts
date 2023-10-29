a57b0fa4905a6520a8a2a0aa616515afc8c2719b
Luckas <luckasranarison@gmail.com>
2023-10-29 20:13:11 +0300

    update: animation method
    use `RequestAnimationFrame`

--- web/src/main.ts
+++ web/src/main.ts
@@ -4,8 +4,7 @@ import "./style.css";
 
 const WIDTH = 64;
 const HEIGHT = 32;
-const TIMERS_REFRESH_RATE = 60;
-const CLOCK_SPEED = 500;
+const STEPS_PER_CYCLE = 10;
 const CELL_SIZE = 6;
 
 const ROMS = ["chip8", "star", "pong", "space-invaders"];
@@ -44,8 +43,41 @@ const displayBuffer = new Uint8Array(
 const canvas = document.getElementById("canvas") as HTMLCanvasElement;
 const ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
 
-let clockInterval: number;
-let timersInterval: number;
+const render = () => {
+  ctx.clearRect(0, 0, WIDTH * CELL_SIZE, HEIGHT * CELL_SIZE);
+
+  for (let i = 0; i < displayBuffer.length; i++) {
+    const row = Math.floor(i / WIDTH);
+    const col = i % WIDTH;
+    const color = displayBuffer[i] == 1 ? "#FFF" : "#000";
+
+    ctx.fillStyle = color;
+    ctx.fillRect(col * CELL_SIZE, row * CELL_SIZE, CELL_SIZE, CELL_SIZE);
+  }
+};
+
+const loop = () => {
+  const executeCycle = () => {
+    for (let i = 0; i < STEPS_PER_CYCLE; i++) {
+      chip.tick();
+    }
+
+    chip.update_timers();
+    render();
+
+    requestAnimationFrame(executeCycle);
+  };
+
+  requestAnimationFrame(executeCycle);
+};
+
+const getRom = async (name: string) => {
+  const rom = await fetch(`/roms/${name}`);
+  const buf = await rom.arrayBuffer();
+  const bytes = new Uint8Array(buf);
+
+  return bytes;
+};
 
 const init = () => {
   const button = document.getElementById("btn-start");
@@ -62,13 +94,10 @@ const init = () => {
   button?.addEventListener("click", async () => {
     const rom = await getRom(select.value);
 
-    clearInterval(clockInterval);
-    clearInterval(timersInterval);
-
     chip.reset();
     chip.load(rom);
 
-    run();
+    loop();
   });
 
   canvas.width = WIDTH * CELL_SIZE;
@@ -87,42 +116,6 @@ const init = () => {
       chip.unset_key();
     }
   });
-};
-
-const getRom = async (name: string) => {
-  const rom = await fetch(`/roms/${name}`);
-  const buf = await rom.arrayBuffer();
-  const bytes = new Uint8Array(buf);
-
-  return bytes;
-};
-
-const render = () => {
-  ctx.clearRect(0, 0, WIDTH * CELL_SIZE, HEIGHT * CELL_SIZE);
-
-  for (let i = 0; i < displayBuffer.length; i++) {
-    const row = Math.floor(i / WIDTH);
-    const col = i % WIDTH;
-    const color = displayBuffer[i] == 1 ? "#FFF" : "#000";
-
-    ctx.fillStyle = color;
-    ctx.fillRect(col * CELL_SIZE, row * CELL_SIZE, CELL_SIZE, CELL_SIZE);
-  }
-};
-
-const run = async () => {
-  clockInterval = setInterval(() => {
-    chip.tick();
-
-    if (chip.has_display_changes()) {
-      render();
-    }
-  }, 1000 / CLOCK_SPEED);
-
-  timersInterval = setInterval(
-    () => chip.update_timers(),
-    1000 / TIMERS_REFRESH_RATE
-  );
 };
 
 init();
